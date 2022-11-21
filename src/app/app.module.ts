94655e3828bf6b05ce8eaa5bcec7bb90e50ec02c
Jari Van de Vel <r0832586@student.thomasmore.be>
2022-11-21 22:39:33 +0100

    layouting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { PatientComponent } from './patient/patient.component';
 import { PatientsComponent } from './patients/patients.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PatientGridComponent } from './patient-grid/patient-grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PatientComponent,
-    PatientsComponent
+    PatientsComponent,
+    PatientGridComponent
   ],
   imports: [
     BrowserModule,
